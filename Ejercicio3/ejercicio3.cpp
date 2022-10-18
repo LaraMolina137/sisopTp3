719d52f74ce85e6ea7715f28a23326319e82c3b7
LaraMolina137 <laramolina122@gmail.com>
2022-10-18 01:25:53 -0300

    cambios

--- Ejercicio3/ejercicio3.cpp
+++ Ejercicio3/ejercicio3.cpp
@@ -41,7 +41,7 @@ int main(int argc, char *argv[]){
     //validar comando de consulta
 
 
-    // while(accion != "quit"){
+    // while(accion != "QUIT"){
 
     //     if(isValidSentence(accion)){
 
