f1f3d816046ad3b3fb3742b1d85b283551f3f8c5
root <root@DESKTOP-7I5UAM3.localdomain>
2022-10-31 22:40:06 -0300

    cambios

--- Ejercicio4/cliente.cpp
+++ Ejercicio4/cliente.cpp
@@ -7,108 +7,238 @@
 #include <sys/types.h>
 #include <cstring>
 #include <fcntl.h>
+#include <sys/sem.h>
+#include <semaphore.h>
+#include <sys/stat.h>
 
 using namespace std;
 
-struct gato{
+struct gato
+{
     char nombre[20];
     char raza[20];
     char sexo;
     bool castrado;
 };
 
-const char* keyAlta = "/tmp/ejercicio4ditommasoalta";
-const char* keyBaja = "/tmp/ejercicio4ditommasobaja";
-const char* keyConsulta = "/tmp/ejercicio4ditommasoconsulta";
+#define SEM_ALTA "/semaltaditommaso"
+#define SEM_BAJA "/sembajaditommaso"
+#define SEM_CONSULTA "/semconsultaditommaso"
+#define SEM_RESP_ALTA "/respaltaditommaso"
+#define SEM_RESP_BAJA "/respbajaditommaso"
+#define SEM_RESP_CONSULTA "/respconsultaditommaso"
 
-string help(){
+#define keyAlta "/tmp/ejercicio4ditommasoalta"
+#define keyBaja "/tmp/ejercicio4ditommasobaja"
+#define keyConsulta "/tmp/ejercicio4ditommasoconsulta"
+#define keyRespAlta "/tmp/ejercicio4ditommasorespalta"
+#define keyRespBaja "/tmp/ejercicio4ditommasorespbaja"
+#define keyRespConsulta "/tmp/ejercicio4ditommasorespconsulta"
+
+sem_t *mutex_baja, *mutexAlta, *mutexConsulta, *mutexRespBaja, *mutexRespAlta, *mutexRespConsulta;
+
+sem_t* openSem(const char* semName,int value);
+int getSharedMemorySegmentId(const char* keyName,int size,int mode);
+void deleteCat(char* nombreGato);
+void consultar(char* nombreGato);
+void recibirRespuestaBaja();
+void recibirRespuestaConsulta();
+
+string help()
+{
     return "Ejecutelo de la siguiente manera: ./ejercicio4 ";
 }
 
-int main(int argc,char *argv[]) {   
-    int shmid;
-    
-    if(argc == 1){
+int main(int argc, char *argv[])
+{
+    if (argc == 1)
+    {
         cout << "Parametros insuficientes" << std::flush;
         return 0;
-    }else{
+    }
+    else
+    {
         string par1 = argv[1];
-        if(argc == 2){
-            if(par1.compare("-h") == 0){
+        if (argc == 2)
+        {
+            if (par1.compare("-h") == 0)
+            {
                 cout << help() << endl;
                 return EXIT_SUCCESS;
-            }else{
-                if(par1.compare("BAJA") != 0 && par1.compare("CONSULTA") != 0){
+            }
+            else
+            {
+                if (par1.compare("BAJA") != 0 && par1.compare("CONSULTA") != 0)
+                {
                     cout << "Parametros incorrectos" << endl;
                     return EXIT_FAILURE;
                 }
             }
-        }else{
-           if(par1.compare("ALTA") != 0 && par1.compare("CONSULTA") != 0 && par1.compare("BAJA") != 0){
+        }
+        else
+        {
+            if (par1.compare("ALTA") != 0 && par1.compare("CONSULTA") != 0 && par1.compare("BAJA") != 0)
+            {
                 cout << "Parametros incorrectos" << endl;
                 return EXIT_FAILURE;
-           }
+            }
         }
-        if(par1.compare("ALTA") == 0){
-                        struct gato *str;
-                key_t key ;
-                 if (-1 != open(keyAlta, 0100, 0777)) {
-                    key = ftok(keyAlta, 0);
-                } else {
-                    perror("open");
-                    exit(1);
-                }
-                if( (shmid = shmget(key,sizeof(struct gato),0644|IPC_CREAT) ) == -1 ){
-                    perror("shmget");
-                    exit(1);
-                }
-
-                str = (gato*) shmat(shmid,(void*)0,0);
-                if (str == (gato *)(-1)) {
+        if (par1.compare("ALTA") == 0)
+        {
+            struct gato *str;
+            key_t key;
+            mutexAlta = openSem(SEM_ALTA,1);
+            sem_wait(mutexAlta);
+            int shmid = getSharedMemorySegmentId(keyAlta,sizeof(struct gato),0644 | IPC_CREAT);
+            if(shmid != -1){
+                str = (gato *)shmat(shmid, (void *)0, 0);
+                if (str == (gato *)(-1))
+                {
                     perror("shmat");
                     exit(1);
                 }
-            
-                strncpy(str->nombre, argv[2],strlen(argv[2]));
-                strncpy(str->raza, argv[3],strlen(argv[3]));
+
+                strcpy(str->nombre, argv[2]);
+                strcpy(str->raza, argv[3]);
                 str->sexo = argv[4][0];
                 str->castrado = argv[5];
 
-                //detach from shared memory 
-            if (shmdt(str) == -1) {
+                // detach from shared memory
+                if (shmdt(str) == -1)
+                {
                     perror("shmdt");
                     exit(1);
                 }
-        }else if (par1.compare("BAJA") == 0){
-             key_t keyB;
-             int shmidB;
-             if (-1 != open(keyBaja, 0100, 0777)) {
-                    keyB = ftok(keyBaja, 0);
-                } else {
-                    perror("open");
-                    exit(1);
-                }
-            if( (shmidB = shmget(keyB,1024,0644|IPC_CREAT) ) == -1 ){
-                perror("shmget");
-                exit(1);
-            }
-            cout << shmidB << endl;
-            char* str;
-             str = (char*) shmat(shmidB,(void*)0,0);
-                if (str == (char *)(-1)) {
-                    perror("shmat");
-                    exit(1);
-                }
-             strncpy(str,argv[2],strlen(argv[2]));   
-                //detach from shared memory 
-            if (shmdt(str) == -1) {
-                    perror("shmdt");
-                    exit(1);
-                }
-        }else if(par1.compare("CONSULTA") == 0){
-            key_t key = ftok(keyConsulta,'R');
+            }  
+            sem_post(mutexAlta);
+        }
+        else if (par1.compare("BAJA") == 0)
+        {
+            key_t keyB;
+            int shmidB;
+            mutex_baja = openSem(SEM_BAJA,1);
+            sem_wait(mutex_baja);
+            deleteCat(argv[2]);
+            sem_post(mutex_baja);
+            recibirRespuestaBaja();
+        }
+        else if (par1.compare("CONSULTA") == 0)
+        {
+            mutexConsulta = openSem(SEM_CONSULTA,1);
+            sem_wait(mutexConsulta);
+            consultar(argv[2]);
+            sem_post(mutexConsulta);
+            mutexRespConsulta = openSem(SEM_RESP_CONSULTA,0);
+            sem_wait(mutexRespConsulta);
+            recibirRespuestaConsulta();
+            sem_post(mutexRespConsulta);
         }
     }
 
     return EXIT_SUCCESS;
+}
+
+sem_t* openSem(const char *semName,int value)
+{
+    sem_t *mutex;
+    if ((mutex = sem_open(semName, O_CREAT, 0644, value)) == SEM_FAILED)
+    {   
+        perror("sem_open");
+        exit(1);
+    }
+    return mutex;
+}
+
+int getSharedMemorySegmentId(const char* keyName,int size,int mode){
+    int shmid;
+    key_t key;
+    if (-1 != open(keyName, 0100, 0777))
+    {
+        key = ftok(keyName, 0);
+    }
+    else
+    {
+        perror("open");
+        return -1;
+    }
+    if ((shmid = shmget(key, size, mode)) == -1)
+    {
+        perror("shmget");
+        return -1;
+    }
+    return shmid;
+}
+
+void deleteCat(char *nombreGato)
+{   
+    cout << "Iniciando baja" << endl;
+    int shmidB = getSharedMemorySegmentId(keyBaja,1024,0644 | IPC_CREAT);
+    char *str = (char *)shmat(shmidB, (void *)0, 0);
+    if (str == (char *)(-1))
+    {
+        perror("shmat");
+        exit(1);
+    }
+    strcpy(str, nombreGato);
+}
+
+void recibirRespuestaBaja()
+{   
+    cout << "Esperando respuesta" << endl;
+    mutexRespBaja = openSem(SEM_RESP_BAJA,0);
+    sem_wait(mutexRespBaja);
+    int shmid = getSharedMemorySegmentId(keyRespBaja,1024,0644);
+
+    char *str = (char *)shmat(shmid, (void *)0, 0);
+    if (str == (char *)(-1))
+    {
+        perror("shmat");
+        exit(1);
+    }
+    cout << str << endl;
+    sem_post(mutex_baja);
+}
+
+void consultar(char* nombreGato){
+    int shmidB = getSharedMemorySegmentId(keyConsulta,1024,0644 | IPC_CREAT);
+    char *str = (char *)shmat(shmidB, (void *)0, 0);
+    if (str == (char *)(-1))
+    {
+        perror("shmat");
+        exit(1);
+    }
+    if(nombreGato != NULL){
+        strcpy(str,nombreGato);
+    }else{
+        strcpy(str,"NOSEPASONOMBRE");
+    }
+    cout << "BUSCANDO: " <<  str << endl;
+}
+
+void recibirRespuestaConsulta()
+{   
+    mutexRespConsulta = openSem(SEM_RESP_CONSULTA,0);
+    sem_wait(mutexRespConsulta);
+    int shmid = getSharedMemorySegmentId(keyRespConsulta,1024,0644 | IPC_CREAT);
+
+    char *str = (char *)shmat(shmid, (void *)0, 0);
+    if (str == (char *)(-1))
+    {
+        perror("shmat");
+        exit(1);
+    }
+    char* ptr = strtok(str,";");
+    while ( ptr != NULL){
+        cout << ptr << endl;
+        ptr = strtok(NULL,";");
+    }
+    if (shmdt(str) == -1)
+    {
+        perror("shmdt");
+    }
+    // destroy the shared memory
+    if (shmctl(shmid, IPC_RMID, NULL) == -1)
+    {
+        perror("shmctl");
+    }
 }
--- Ejercicio4/servidor.cpp
+++ Ejercicio4/servidor.cpp
@@ -8,143 +8,295 @@
 #include <chrono>
 #include <thread>
 #include <fcntl.h>
+#include <filesystem>
+#include <semaphore.h>
+#include <sys/stat.h>
+#include <cstring>
 
 using namespace std;
 
-const char* keyAlta = "/tmp/ejercicio4ditommasoalta";
-const char* keyBaja = "/tmp/ejercicio4ditommasobaja";
-const char* keyConsulta = "/tmp/ejercicio4ditommasoconsulta";
+#define SEM_ALTA "/semaltaditommaso"
+#define SEM_BAJA "/sembajaditommaso"
+#define SEM_CONSULTA "/semconsultaditommaso"
+#define SEM_RESP_ALTA "/respaltaditommaso"
+#define SEM_RESP_BAJA "/respbajaditommaso"
+#define SEM_RESP_CONSULTA "/respconsultaditommaso"
 
+#define keyAlta "/tmp/ejercicio4ditommasoalta"
+#define keyBaja "/tmp/ejercicio4ditommasobaja"
+#define keyConsulta "/tmp/ejercicio4ditommasoconsulta"
+#define keyRespAlta "/tmp/ejercicio4ditommasorespalta"
+#define keyRespBaja "/tmp/ejercicio4ditommasorespbaja"
+#define keyRespConsulta "/tmp/ejercicio4ditommasorespconsulta"
 
-struct gato{
+sem_t *mutexBaja, *mutexAlta, *mutexConsulta, *mutexRespBaja, *mutexRespAlta, *mutexRespConsulta;
+
+struct gato
+{
     char nombre[20];
     char raza[20];
     char sexo;
     bool castrado;
 };
 
+template <typename T>
 
-int main(int argc, const char * argv[]) {
-    while(true){ 
-    //ALTA
-
-    int shmidA = -1,shmidB = -1,shmidC = -1; 
-    // ftok to generate unique key
-     
-    key_t key;
-    
-    if (-1 != open(keyAlta, O_CREAT, 0777)) {
-        key = ftok(keyAlta, 'R');
-     } else {
-        perror("open");
-        exit(1);
-    }
-    // shmget returns an identifier in shmid
-    struct gato *str;
-    if(( shmidA = shmget(key,sizeof(struct gato),0644) ) == -1 ) {
-         perror("shmget");
-    }else{
-        // shmat to attach to shared memory
-        cout << shmidA << endl ; 
-        str = (gato*) shmat(shmidA,(void*)0,0);
-        if (str == (gato *)(-1)) {
-            perror("shmat");
-        }
-        cout << "sexo:  " << str->sexo << endl;
-        cout << "castrado:  " << str->castrado << endl ;
-        cout << "nombre:  " << str->nombre << endl;
-        cout << "raza:  " << str->raza << endl << std::flush ;
-
-        ofstream outfile;
-
-        outfile.open("dbgatitos.txt", ios::out | ios::app);
-
-        outfile << str->nombre << ","<< str->raza << "," << str->sexo<< "," << str->castrado<< endl;
-        
-        //detach from shared memory 
-        if (shmdt(str) == -1) {
-            perror("shmdt");
-        }
-        // destroy the shared memory
-        if(shmctl(shmidA,IPC_RMID,NULL) == -1){
-            perror("shmctl");
-        }
-    }
-
-
-    //BAJA
-    key_t keyB ;
-    if (-1 != open(keyBaja, 0100, 0777)) {
-                    keyB = ftok(keyBaja, 0);
-                } else {
-                    perror("open");
-                    exit(1);
-                }
-    // shmget returns an identifier in shmid
-    char* nombreGato;
-    if(( shmidB = shmget(keyB,1024,0644|IPC_CREAT) ) == -1 ) {
-         perror("shmget");
-    }else{
-    // shmat to attach to shared memory
-    nombreGato = (char*) shmat(shmidB,(void*)0,0);
-    if (nombreGato == (char *)(-1)) {
-        perror("shmat");
-    }else{
-        string line;
-        ifstream iFile;
-        cout << nombreGato << endl;
-        iFile.open("dbgatitos.txt");
-        unsigned int curLine = 0;
-        while(getline(iFile, line)) {
-                curLine++;
-            if (line.find(nombreGato, 0) != string::npos) {
-               // cout << "found: " << nombreGato << "line: " << curLine << endl;
-            }
-        }
-         ofstream outfile;
-         outfile.open("dbgatitos.txt", ios::out | ios::app);
-    }   
-    //detach from shared memory 
-    if (shmdt(nombreGato) == -1) {
+void detachAndDestroy(T toDetach, int toDestroy)
+{
+    // detach from shared memory
+    if (shmdt(toDetach) == -1)
+    {
         perror("shmdt");
     }
-    
     // destroy the shared memory
-    if(shmctl(shmidB,IPC_RMID,NULL) == -1){
+    if (shmctl(toDestroy, IPC_RMID, NULL) == -1)
+    {
         perror("shmctl");
     }
-    }
-    
-    
+}
 
-    //CONSULTA
+sem_t *openSem(const char *semName);
+int getSharedMemorySegmentId(const char *keyName, int size, int mode);
+// void detachAndDestroy(char* toDetach,int toDestroy);
 
-    key_t keyC = ftok(keyConsulta,'R');
-    
-    // shmget returns an identifier in shmid
-    char* consulta;
-    if((shmidC = shmget(keyC,1024,0644|IPC_CREAT) ) == -1 ) {
-         perror("shmget");
-    }else{
-            // shmat to attach to shared memory
-        consulta = (char*) shmat(shmidC,(void*)0,0);
-        if (consulta == (char *)(-1)) {
-            perror("shmat");
+int main(int argc, const char *argv[])
+{
+    mutexBaja = sem_open(SEM_BAJA, O_CREAT, 0644, 1);
+    sem_destroy(mutexBaja);
+    while (true)
+    {
+        // ALTA
+
+        int shmidA = -1, shmidB = -1, shmidC = -1;
+        // ftok to generate unique key
+
+        if ((mutexAlta = sem_open(SEM_BAJA, O_CREAT, 0644, 1)) == SEM_FAILED)
+        {
+            perror("sem_open");
         }
+        sem_wait(mutexAlta);
+        key_t key;
 
-        //detach from shared memory 
-        if (shmdt(consulta) == -1) {
-            perror("shmdt");
+        if (-1 != open(keyAlta, O_CREAT, 0777))
+        {
+            key = ftok(keyAlta, 0);
+        }
+        else
+        {
+            perror("open");
+        }
+        // shmget returns an identifier in shmid
+        struct gato *str;
+        if ((shmidA = shmget(key, sizeof(struct gato), 0644)) == -1)
+        {
+            perror("shmget");
+        }
+        else
+        {
+            // shmat to attach to shared memory
+            cout << shmidA << endl;
+            str = (gato *)shmat(shmidA, (void *)0, 0);
+            if (str == (gato *)(-1))
+            {
+                perror("shmat");
             }
-         // destroy the shared memory
-        if(shmctl(shmidC,IPC_RMID,NULL) == -1){
-            perror("shmctl");
-            exit(1);
-        }     
+            cout << "sexo:  " << str->sexo << endl;
+            cout << "castrado:  " << str->castrado << endl;
+            cout << "nombre:  " << str->nombre << endl;
+            cout << "raza:  " << str->raza << endl
+                 << std::flush;
+
+            ofstream outfile;
+
+            outfile.open("dbgatitos.txt", ios::out | ios::app);
+
+            outfile << str->nombre << "," << str->raza << "," << str->sexo << "," << str->castrado << ";" << endl;
+            outfile.flush();
+            outfile.close();
+            detachAndDestroy<gato *>(str, shmidA);
+            // detach from shared memory
+        }
+        sem_post(mutexAlta);
+
+        ///////// BAJA ///////////////
+        key_t keyB;
+        char *nombreGato;
+        mutexBaja = openSem(SEM_BAJA);
+        sem_wait(mutexBaja);
+        shmidB = getSharedMemorySegmentId(keyBaja, 1024, 0644);
+
+        // shmget returns an identifier in shmid
+        if (shmidB != -1)
+        {
+            // shmat to attach to shared memory
+            nombreGato = (char *)shmat(shmidB, (void *)0, 0);
+            if (nombreGato == (char *)(-1))
+            {
+                perror("shmat");
+            }
+            else
+            {
+                string line;
+                ifstream iFile;
+                ofstream outfile;
+                outfile.open("dbgatitostmp.txt", ios::out | ios::app);
+                iFile.open("dbgatitos.txt");
+                unsigned int curLine = 0;
+                unsigned int foundit = 0;
+                while (getline(iFile, line))
+                {
+                    curLine++;
+                    string tofind = nombreGato;
+                    if (line.find(tofind + ",", 0) != string::npos)
+                    {
+                        cout << "found: " << nombreGato << "line: " << curLine << endl;
+                        foundit = 1;
+                    }
+                    else
+                    {
+                        outfile << line << endl;
+                    }
+                }
+                iFile.close();
+                outfile.close();
+                remove("dbgatitos.txt");
+                rename("dbgatitostmp.txt", "dbgatitos.txt");
+                mutexRespBaja = openSem(SEM_RESP_BAJA);
+                int shmidRespB = getSharedMemorySegmentId(keyRespBaja, 1024, 0644);
+                char *respBaja = (char *)shmat(shmidRespB, (void *)0, 0);
+                if (respBaja == (char *)(-1))
+                {
+                    perror("shmat");
+                }
+                if (foundit == 1)
+                {
+                    strcpy(respBaja, "BORRADO");
+                }
+                else
+                {
+                    strcpy(respBaja, "NO EXISTE");
+                }
+                sem_post(mutexRespBaja);
+            }
+            detachAndDestroy<char *>(nombreGato, shmidB);
+        }
+        sem_post(mutexBaja);
+
+        ////////// CONSULTA //////////////////////////////////////
+
+        key_t keyC;
+        char *consulta;
+        mutexConsulta = openSem(SEM_CONSULTA);
+        sem_wait(mutexConsulta);
+        shmidC = getSharedMemorySegmentId(keyConsulta, 1024, 0644);
+        if (shmidC != -1)
+        {
+            // shmat to attach to shared memory
+            consulta = (char *)shmat(shmidC, (void *)0, 0);
+            if (consulta == (char *)(-1))
+            {
+                perror("shmat");
+            }
+            key_t keyRespCons;
+            char *respConsulta;
+            mutexRespConsulta = openSem(SEM_RESP_CONSULTA);
+            int shmidRespC = getSharedMemorySegmentId(keyRespConsulta, 1024, 0644 | IPC_CREAT);
+            if (shmidRespC != -1)
+            {
+
+                respConsulta = (char *)shmat(shmidRespC, (void *)0, 0);
+                if (respConsulta == (char *)(-1))
+                {
+                    perror("shmat");
+                }
+                string totalFile;
+                string line;
+                ifstream iFile;
+                iFile.open("dbgatitos.txt");
+                string cons = consulta;
+                if (cons.compare("NOSEPASONOMBRE") != 0)
+                {
+                    unsigned int foundit = 0;
+                    while (foundit == 0 && getline(iFile, line))
+                    {
+                        string tofind = consulta;
+                        if (line.find(tofind + ",", 0) != string::npos)
+                        {
+                            totalFile.append(line);
+                            foundit = 1;
+                        }
+                    }
+                    if (foundit == 0)
+                    {
+                        totalFile = "No se encontro al gatito";
+                    }
+                }
+                else
+                {
+                    while (getline(iFile, line))
+                    {
+                        totalFile.append(line);
+                    }
+                    cout << totalFile.c_str() << endl;
+                }
+                strcpy(respConsulta, totalFile.c_str());
+                iFile.close();
+                if (shmdt(respConsulta) == -1)
+                {
+                    perror("shmdt");
+                }
+            }
+            detachAndDestroy<char *>(consulta, shmidC);
+            sem_post(mutexRespConsulta);
+        }
+        sem_post(mutexConsulta);
+        cout << "------------------" << endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(5000));
     }
-    
-   
-    std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+    return EXIT_SUCCESS;
 }
-    return EXIT_SUCCESS;  
+
+sem_t *openSem(const char *semName)
+{
+    sem_t *mutex;
+    if ((mutex = sem_open(semName, O_CREAT, 0644, 1)) == SEM_FAILED)
+    {
+        perror("sem_open");
+        return NULL;
+    }
+    return mutex;
 }
+
+int getSharedMemorySegmentId(const char *keyName, int size, int mode)
+{
+    int shmid;
+    key_t key;
+    if (-1 != open(keyName, 0100, 0777))
+    {
+        key = ftok(keyName, 0);
+    }
+    else
+    {
+        perror("open");
+        return -1;
+    }
+    if ((shmid = shmget(key, size, mode)) == -1)
+    {
+        perror("shmget");
+        return -1;
+    }
+    return shmid;
+}
+
+/* void detachAndDestroy(char* toDetach,int toDestroy){
+    // detach from shared memory
+    if (shmdt(toDetach) == -1)
+    {
+        perror("shmdt");
+    }
+    // destroy the shared memory
+    if (shmctl(toDestroy, IPC_RMID, NULL) == -1)
+    {
+        perror("shmctl");
+    }
+} */
